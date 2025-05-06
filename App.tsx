edd5b114be2dc7e0279c35182ef5988fd09bded0
Nitin Varda <nitinvarda@Nitins-Fellow-MacBook-Pro.local>
2025-05-05 19:00:02 -0700

    added github

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function App() {
   return (
     <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
-      <Text style={{color:'black'}} >This is testing fastlane app through github actions</Text>
+      <Text style={{color:'black'}} >This is testing fastlane app through github actions with fastlane</Text>
     </View>
   )
 }
